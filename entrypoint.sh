9c0c029dbbcb0a25cface2b5417da94e0df50d1c
Chibuike Onuigwe <chibuike.onuigwe@yahoo.com>
2021-05-31 15:00:40 -0400

    Testing differential checks

--- entrypoint.sh
+++ entrypoint.sh
@@ -79,6 +79,8 @@ else
   HEAD_REF=$(git rev-parse ${GITHUB_HEAD_REF/#/'origin/'})
   DIFF_FILES=$(git diff --diff-filter=d --name-only $BASE_REF $HEAD_REF | tr '\n' ' ')
 
+  cat $DIFF_FILES
+  
   IFS=' ' read -r -a files2scan <<< "$DIFF_FILES"
 
   SCAN_FILES_FLAG=""
@@ -92,8 +94,7 @@ else
       SCAN_FILES_FLAG="$SCAN_FILES_FLAG -f $f"
     done
     checkov $SCAN_FILES_FLAG $CHECK_FLAG $SKIP_CHECK_FLAG $QUIET_FLAG $SOFT_FAIL_FLAG $FRAMEWORK_FLAG $EXTCHECK_DIRS_FLAG $EXTCHECK_REPOS_FLAG $OUTPUT_FLAG $DOWNLOAD_EXTERNAL_MODULES_FLAG > checkov_stdout
-    cat checkov_stdout
-
+    
     CHECKOV_EXIT_CODE=$?
 
     #echo "::set-output name=<checkov>::$(cat checkov_stdout)"
