3a53121940f113b0835a6c368baeba40641ac154
Chibuike Onuigwe <chibuike.onuigwe@yahoo.com>
2021-06-02 16:43:41 -0400

    Testings

--- entrypoint.sh
+++ entrypoint.sh
@@ -75,10 +75,10 @@ echo "::add-matcher::checkov-problem-matcher.json"
   #CHECKOV_EXIT_CODE=$?
 #fi
 
-checkov -d $INPUT_DIRECTORY $CHECK_FLAG $SKIP_CHECK_FLAG $QUIET_FLAG $SOFT_FAIL_FLAG $FRAMEWORK_FLAG $EXTCHECK_DIRS_FLAG $EXTCHECK_REPOS_FLAG $OUTPUT_FLAG $DOWNLOAD_EXTERNAL_MODULES_FLAG > checkov_stdout_fail
+checkov -d $INPUT_DIRECTORY $CHECK_FLAG $SKIP_CHECK_FLAG $QUIET_FLAG $SOFT_FAIL_FLAG $FRAMEWORK_FLAG $EXTCHECK_DIRS_FLAG $EXTCHECK_REPOS_FLAG $OUTPUT_FLAG $DOWNLOAD_EXTERNAL_MODULES_FLAG > checkov_stdout
   
 CHECKOV_EXIT_CODE=$?
-echo "::set-output name=<checkov>::$(cat checkov_stdout_fail)"
+echo "::set-output name=<checkov>::$(cat checkov_stdout)"
 
 if [ ! -z "$INPUT_DOWNLOAD_EXTERNAL_MODULES" ] && [ "$INPUT_DOWNLOAD_EXTERNAL_MODULES" = "true" ]; then
   echo "Cleaning up $INPUT_DIRECTORY/.external_modules directory"
