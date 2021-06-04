4dcedf3797bb42727f51ae681bccdf1d4b31b575
Chibuike Onuigwe <chibuike.onuigwe@yahoo.com>
2021-06-04 12:35:43 -0400

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 # Actions pass inputs as $INPUT_<input name> environment variables
 #
 
-warnings_file= /warnings.rule | paste -sd " "
-failure_file= /failure.rule | paste -sd " "
+warnings_file= /usr/local/lib/warnings.rule | paste -sd " "
+failure_file= /usr/local/lib/failure.rule | paste -sd " "
 
 echo $warnings_file
 echo $failure_file
